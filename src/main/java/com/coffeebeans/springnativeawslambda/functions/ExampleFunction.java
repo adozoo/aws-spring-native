dfab5178dc6795235bbcc140b1d4cafba57ac57f
andre <adozoo.c@gmail.com>
2022-07-05 15:43:10 +0900

    andre

--- src/main/java/com/coffeebeans/springnativeawslambda/functions/ExampleFunction.java
+++ src/main/java/com/coffeebeans/springnativeawslambda/functions/ExampleFunction.java
@@ -2,6 +2,9 @@ package com.coffeebeans.springnativeawslambda.functions;
 
 import com.coffeebeans.springnativeawslambda.model.Request;
 import com.coffeebeans.springnativeawslambda.model.Response;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Function;
 import lombok.extern.slf4j.Slf4j;
 
@@ -12,9 +15,18 @@ public class ExampleFunction implements Function<Request, Response> {
   public Response apply(final Request request) {
     log.info("Converting request into a response...");
 
+    List<String> stringList = new ArrayList<>();
+
+    stringList.add("A234567890ABC");
+    stringList.add("B234567890ABC");
+    stringList.add("C234567890ABC");
+    stringList.add("D234567890ABC");
+    stringList.add("E234567890ABC");
+
     final Response response = Response.builder()
-        .name(request.getName())
+        .rank(request.getName())
         .saved(true)
+        .janList(stringList)
         .build();
 
     log.info("Converted request into a response.");
--- src/main/java/com/coffeebeans/springnativeawslambda/model/Response.java
+++ src/main/java/com/coffeebeans/springnativeawslambda/model/Response.java
@@ -3,6 +3,7 @@ package com.coffeebeans.springnativeawslambda.model;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonProperty.Access;
 import java.io.Serializable;
+import java.util.List;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -18,9 +19,13 @@ public class Response implements Serializable {
 
   @NotBlank
   @JsonProperty(access = Access.READ_ONLY)
-  private String name;
+  private String rank;
 
   @NotNull
   @JsonProperty(access = Access.READ_ONLY)
   private boolean saved;
+
+  @NotNull
+  @JsonProperty(access = Access.READ_ONLY)
+  private List<String> janList;
 }
